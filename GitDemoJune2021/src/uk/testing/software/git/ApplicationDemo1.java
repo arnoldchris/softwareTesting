e700eeea36740f54b2176149146df0ecb1732beb
chris <arnold937@yahoo.com>
2021-06-28 10:11:20 +0100

    Jira - SFT -1234 Adding two new classes

---
+++ GitDemoJune2021/src/uk/testing/software/git/ApplicationDemo1.java
@@ -0,0 +1,8 @@
+package uk.testing.software.git;
+
+public class ApplicationDemo1 {
+
+    public void doSomething(){
+        System.out.println("Application Class Demo1 Running");
+    }
+}
---
+++ GitDemoJune2021/src/uk/testing/software/git/ApplicationDemo2.java
@@ -0,0 +1,8 @@
+package uk.testing.software.git;
+
+public class ApplicationDemo2 {
+
+    public void doSomething(){
+        System.out.println("Application Class Demo2 is Running");
+    }
+}
